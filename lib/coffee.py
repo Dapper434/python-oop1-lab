5d7d3c4e411f05c1a3c0abf855336c0924edcc95
Pipsy <gpflaslik7112@gmail.com>
2026-06-14 01:35:10 +0300

    initial commit of project files

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    """Represents a coffee order with size and price."""
+
+    VALID_SIZES = {"Small", "Medium", "Large"}
+
+    def __init__(self, size: str, price: float):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self) -> str:
+        return self._size
+
+    @size.setter
+    def size(self, value: str):
+        if value not in self.VALID_SIZES:
+            print("size must be Small, Medium, or Large")
+            return
+        self._size = value
+
+    def tip(self) -> None:
+        print("This coffee is great, here's a tip!")
+        self.price += 1
